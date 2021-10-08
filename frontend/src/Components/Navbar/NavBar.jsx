0fdcaecae950ec4ef9185216f56decc3b51b6f38
kamal <kamaldro97@gmail.com>
2021-10-08 23:13:06 +0530

    NavBar started

---
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NavBar.module.css';
+
+export const NavBar = () => {
+    return (
+        <div className={styles.NavBarContainer}>
+            <div className={styles.NavBarItemContainerSmall}>
+
+            </div>
+            <div className={styles.NavBarItemContainerLarge}>
+
+            </div>
+            
+        </div>
+    )
+}
